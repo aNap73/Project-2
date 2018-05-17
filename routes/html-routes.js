37aa1c56141a673663fdf8a70af28e72f9aee8f3
Jason Gomez <jason.gomez@me.com>
2018-05-17 19:38:37 -0400

    added catch

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,7 +7,7 @@ var path = require("path");
 var passport = require("../config/passport");
 var db = require("../models");
 /* Routes */
-module.exports = function(app) {
+module.exports = function (app) {
 
   var ex = {
     name: "test"
@@ -15,26 +15,32 @@ module.exports = function(app) {
 
   /* Homepage route */
 
-  app.get("/" ,function(req , res){  
-  
-  db.users.create({
-    email:'napolitano.anthony@yahoo.com',
-    password:'password123',
-    onBoardId:1,
-    userImage:'https://anap73.github.io/Bootstrap-Portfolio.github.io/assets/images/AntMeHead.png'
-    }).then(function(outdata){
-      console.log('outdata',outdata.dataValues.password)
-     //VALIDPASSWORD EXAMPLE; IF TRUE THEN VALIDATE USER
+  app.get("/", function (req, res) {
+
+    db.users.create({
+      email: 'napolitano.anthony@yahoo.com',
+      password: 'password123',
+      onBoardId: 1,
+      userImage: 'https://anap73.github.io/Bootstrap-Portfolio.github.io/assets/images/AntMeHead.png'
+    }).then(function (outdata) {
+      console.log('outdata', outdata.dataValues.password)
+      //VALIDPASSWORD EXAMPLE; IF TRUE THEN VALIDATE USER
       console.log('password hash working:', outdata.validPassword('password123'));
-       req.login({email:'napolitano.anthony@yahoo.com', password:'password123'}, function (err) {
-      if (err) {      
-        console.log(err);      
-        return res.status(422).json(err);
-      }
-      res.json(req.user);
-  }); 
+      req.login({
+        email: 'napolitano.anthony@yahoo.com',
+        password: 'password123'
+      }, function (err) {
+        if (err) {
+          console.log(err);
+          return res.status(422).json(err);
+        }
+        res.json(req.user);
+      });
+
+    }).catch(err => {
+      console.log(err);
+    });
 
   });
-    
-  
+
 }
