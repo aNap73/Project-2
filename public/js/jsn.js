19831da0b4097f2441fd2fb348585ba54ebfe757
Jason Gomez <jason.gomez@me.com>
2018-05-19 11:47:55 -0400

    js for the admin page

---
+++ public/js/jsn.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+  /* Get request for getting an article */
+  $("#articles-button").click(function (e) {
+    e.preventDefault();
+  });
+
+  /* Get request for getting a user */
+  $("#user-button").click(function (e) {
+    e.preventDefault();
+  });
+
+  /* Get request for getting a post */
+  $("post-button").click(function (e) {
+    e.preventDefault();
+  });
+
+});
