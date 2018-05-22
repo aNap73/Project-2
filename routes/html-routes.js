f685523887eb30888a8eda6f07cd420c4edbe129
aNap73 <napolitano.anthony@yahoo.com>
2018-05-21 20:09:28 -0400

    finished registration: outstanding bug name not logged in after eula accept

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,10 +6,44 @@ var path = require("path");
 
 var passport = require("../config/passport");
 var db = require("../models");
+
 /* Routes */
 module.exports = function (app) {
-
-  app.get("/register", function (req, res) {
+   app.get("/autologin/:autoid/:email", function(req,res){
+    
+    let pageData = {
+      happyMonkeys: 'Bill'
+    };
+    if(req.params.autoid&&req.params.email){
+      console.log('AUTOLOG',req.params.autoid,req.params.email)
+      db.users.findOne({
+        where: {
+          email: req.params.email
+        }
+      }).then(function(data){
+         if(data.onBoardId.toString()===req.params.autoid.toString()){
+          data.update(
+            { hasEmailConfirmed: true,
+              hasAcceptedTerms: true,
+              onBoardId: null },
+            { where:{email: req.params.email}}
+          ).then(function(data){
+            console.log('before login');
+            
+            req.login(data.email, function(err) {
+              console.log('before redirect');
+              res.redirect("/");
+              return;
+  
+            });
+          });
+         }
+      });
+    } 
+    
+    
+   });
+   app.get("/register", function (req, res) {
 
     let pageData = {
       regInit: {
@@ -18,9 +52,9 @@ module.exports = function (app) {
     };
     res.render("register", pageData);
   });
-
-  app.get("/register/:regCode", function (req, res) {
-    console.log('regCode: ', req.params.regCode);
+  app.get("/register/:regCode/", function (req, res) {
+    // console.log('regCodeIn: ', req.params.regCode);
+    // console.log('req.query.emailaddy', req.query.emailaddy);
     let pageData = {};
     if (req.params.regCode) {
       switch (req.params.regCode) {
@@ -45,20 +79,55 @@ module.exports = function (app) {
           };
           res.render("register", pageData);
           break;
+
         default:
-          pageData.regFail = {
-            value: true
-          };
-          res.render("register", pageData);
-          break;
+          
+          //check 4 email
+          if (req.query.emailaddy) {
 
+            //get user record
+            db.users.findOne({
+              where: {
+                email: req.query.emailaddy
+              }
+            }).then(function (data) {
+              if (data) {
+                //if code matches update user record to hasEmailValidated  
+                if (data.onBoardId.toString() === req.params.regCode.toString()) {
+                  data.update(
+                    { hasEmailConfirmed: true },
+                    { where:{email: data.email}}
+                  ).then(function(data){
+                    let pageData2 = {
+                      email: req.query.emailaddy,
+                      autosecid: req.params.regCode  
+                    };                    
+                    console.log('rendering eula', pageData2);
+                    res.render("eula", pageData2);
+                    return;
+                  });
+                  } else {
+                    pageData.regFail = {
+                      value: true
+                    };
+                    res.render("register", pageData);
+                    return;
+                  };
+                } else {
+                pageData.regFail = {
+                  value: true
+                };
+                res.render("register", pageData);
+                return;
+              }
+            })
+          } else {
+            res.render("register", pageData);
+            return;
+          }
+          
       }
-
     }
-    //CREATEUPDATE USER by EMAIL with new 
-    //
-    //SEND EMAIL
-
   });
   app.get("/logout", function (req, res) {
     let pageData = {};
@@ -78,37 +147,34 @@ module.exports = function (app) {
       pageData = {
         name: req.user.email,
         userObj: req.user,
-        happyMonkeys:'Bill'
+        happyMonkeys: 'Bill'
       };
-      if(req.user.hasAdmin){
-        res.render("admin",pageData);
-      }else {
+      if (req.user.hasAdmin) {
+        res.render("admin", pageData);
+      } else {
         res.render("index", pageData);
       };
-    }else{
-        res.render("index", pageData);
-      }
-
-        
-    
-    
+    } else {
+      res.render("index", pageData);
+    }
   });
 
   /* Homepage route */
   app.get("*", function (req, res) {
     let pageData = {};
     pageData = {
-      
-      happyMonkeys:'Bill'
+      happyMonkeys: 'Bill'
     };
+
     if (req.user) {
-      pageData.happyMonkeys= 'Bill Logged in';
+      pageData.happyMonkeys = 'Bill Logged in';
       pageData.name = req.user.email;
       pageData.userObj = req.user;
     }
-    
     res.render("index", pageData);
   });
+
+
 
 
 }
